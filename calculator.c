d565af71d842c4a8d69cad01d4143c270d82f02f
sp-b18ak <95131952+sp-b18ak@users.noreply.github.com>
2022-07-27 23:26:28 +0530

    Calculator - Addition

---
+++ calculator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter two numbers - ");
+    scanf("%d%d",&a,&b);
+
+    int result = a + b;
+
+    printf("\nThe result is - %d",result);
+}
