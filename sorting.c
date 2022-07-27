41b5df0214469da5ace2d52ad3e999bd53cc0d7b
sp-b18ak <95131952+sp-b18ak@users.noreply.github.com>
2022-07-28 00:08:57 +0530

    updated Quick sort

--- sorting.c
+++ sorting.c
@@ -6,7 +6,7 @@
 
 void fswap(int arr[],int a, int b);
 int p(int arr[],int l, int m, int k);
-void fsort(int arr[], int n);
+void fsort(int arr[], int l,int r);
 void print(int arr[], int n);
   
 int main()
